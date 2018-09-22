e531f23822e19fabc43f6f5b747423be20948ef0
ammagamma <contactammagamma@gmail.com>
2018-09-22 09:08:34 +0200

    Shows error in case token is missing.

--- deck-gl/token.js
+++ deck-gl/token.js
@@ -1,1 +1,6 @@
 export const token = ''; // set your token here
+
+if (token.length === 0) {
+    console.error('you need to set your token in src/token.js');
+    alert('you need to set your token in src/token.js');
+}
